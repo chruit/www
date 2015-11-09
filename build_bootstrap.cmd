3f31a5880fe9d4ee049cf5428191fde8be356462
chruit <chruit@uw.edu>
2015-11-09 14:17:58 -0800

    add flag for overwriting on a copy

--- build_bootstrap.cmd
+++ build_bootstrap.cmd
@@ -4,7 +4,7 @@ copy variables.less bootstrap\less\variables.less
 echo "changing to bootstrap dir for build"
 cd bootstrap
 call grunt
-copy dist\css\* ..\chru_dot_edu\chru_dot_edu\static\css
+copy /Y dist\css\* ..\chru_dot_edu\chru_dot_edu\static\css
 echo "returning to dir"
 cd ..
 echo "done building css for CHRU webpage"
