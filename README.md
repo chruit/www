9359cbbec0f552f52c8f705cc832498acc0205c2
chruit <chruit@uw.edu>
2015-11-09 13:40:18 -0800

    Move bootstrap to a submodule Move variables.less Add methods to build bootstrap via Windows Fix simple layout bugs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # www
+
+# The webpage for CHRU
+
+# build upon Bootstrap v3.3.5 as a submodule
+
+# use a custom variables.less file to apply custom style
+
--- build_bootstrap.cmd
+++ build_bootstrap.cmd
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash
 
 echo "copy variables.less file"
-cp variables.less bootstrap/less/
+copy variables.less bootstrap\less\variables.less
 echo "changing to bootstrap dir for build"
 cd bootstrap
-grunt
-cp dist/css/* ../chru_dot_edu/chru_dot_edu/static/css/
+call grunt
+copy dist\css\* ..\chru_dot_edu\chru_dot_edu\static\css
 echo "returning to dir"
 cd ..
 echo "done building css for CHRU webpage"
